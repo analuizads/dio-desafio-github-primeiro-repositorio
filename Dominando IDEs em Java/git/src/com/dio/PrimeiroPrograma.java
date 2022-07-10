7cfc53e3f3edf4d64a3eefd36705b2f8f7a17f99
analuizads <anadiniz.ds@gmail.com>
2022-07-10 01:36:48 -0300

    Commit teste Eclipse

---
+++ Dominando IDEs em Java/git/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//System.out.println("Hello World!");
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+
+	}
+
+}
